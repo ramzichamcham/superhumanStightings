d73d09958451b0af29793479c8ba3331a1dca4a2
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-08 12:10:42 -0500

    Project Initialization and database connectivity setup

---
+++ src/main/java/com/sg/superhumanSightings/SuperhumanSightingsApplication.java
@@ -0,0 +1,13 @@
+package com.sg.superhumanSightings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperhumanSightingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperhumanSightingsApplication.class, args);
+	}
+
+}
