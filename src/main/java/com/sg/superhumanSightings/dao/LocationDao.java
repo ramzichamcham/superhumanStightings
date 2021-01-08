c657574ca70d61a3de69f0e4879cd75a89ade7b8
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-08 17:08:14 -0500

    Update LocationDao

--- src/main/java/com/sg/superhumanSightings/dao/LocationDao.java
+++ src/main/java/com/sg/superhumanSightings/dao/LocationDao.java
@@ -1,4 +1,17 @@
 package com.sg.superhumanSightings.dao;
 
+import com.sg.superhumanSightings.entity.Location;
+import com.sg.superhumanSightings.entity.Superhuman;
+
+import java.util.List;
+
 public interface LocationDao {
+
+    Location getLocationById(int id);
+    List<Location> getAllLocations();
+    Location addLocation(Location org);
+    void updateLocation(Location org);
+    void deleteLocation(Location org);
+
+    List<Location> getLocationsForSuperhuman(Superhuman sh);
 }
