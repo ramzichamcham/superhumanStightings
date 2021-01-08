80907e610c366a377ac859cf37ff2681ea4884a4
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-08 17:14:18 -0500

    Update SightingDao.java

--- src/main/java/com/sg/superhumanSightings/dao/SightingDao.java
+++ src/main/java/com/sg/superhumanSightings/dao/SightingDao.java
@@ -5,9 +5,10 @@ import com.sg.superhumanSightings.entity.Sighting;
 import com.sg.superhumanSightings.entity.Superhuman;
 
 import java.sql.Date;
+import java.util.List;
 
 public interface SightingDao {
 
     Sighting addSighting(Superhuman sh, Location loc, Date date);
-
+    List<Sighting> getSightingsForDate(Date date);
 }
