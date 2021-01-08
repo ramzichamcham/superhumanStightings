145256e93b83f980adaa3648adcb68dae25fe509
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-08 13:02:52 -0500

    Add Superhuman entity

---
+++ src/main/java/com/sg/superhumanSightings/entity/Superhuman.java
@@ -0,0 +1,50 @@
+package com.sg.superhumanSightings.entity;
+
+import java.util.Objects;
+
+public class Superhuman {
+
+    private int id;
+    private String name;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //description can be null
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Superhuman that = (Superhuman) o;
+        return id == that.id &&
+                name.equals(that.name) &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
+}
