090c10edc6bb9ac1782139b6b5e435b52c74996f
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-10 15:18:35 -0500

    add id property to sighting

--- src/main/java/com/sg/superhumanSightings/entity/Sighting.java
+++ src/main/java/com/sg/superhumanSightings/entity/Sighting.java
@@ -1,12 +1,25 @@
 package com.sg.superhumanSightings.entity;
 
+import org.apache.tomcat.jni.Local;
+
 import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Sighting {
+    private int id;
     private Superhuman superhuman;
     private Location location;
     private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public Superhuman getSuperhuman() {
         return superhuman;
@@ -32,19 +45,19 @@ public class Sighting {
         this.date = date;
     }
 
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Sighting sighting = (Sighting) o;
-        return superhuman.equals(sighting.superhuman) &&
+        return id == sighting.id &&
+                superhuman.equals(sighting.superhuman) &&
                 location.equals(sighting.location) &&
                 date.equals(sighting.date);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(superhuman, location, date);
+        return Objects.hash(id, superhuman, location, date);
     }
 }
