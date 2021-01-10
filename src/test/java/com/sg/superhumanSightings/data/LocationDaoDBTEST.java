fb121efc4e332367fbea27a2e147cd6eb10e0016
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-10 16:18:21 -0500

    Add and Setup dao test files

---
+++ src/test/java/com/sg/superhumanSightings/data/LocationDaoDBTEST.java
@@ -0,0 +1,69 @@
+package com.sg.superhumanSightings.data;
+
+import com.sg.superhumanSightings.dao.*;
+import com.sg.superhumanSightings.entity.*;
+import net.bytebuddy.implementation.bind.annotation.Super;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LocationDaoDBTEST {
+
+    @Autowired
+    LocationDao locationDao;
+
+    @Autowired
+    OrganizationDao organizationDao;
+
+    @Autowired
+    SightingDao sightingDao;
+
+    @Autowired
+    SuperhumanDao superhumanDao;
+
+    @Autowired
+    SuperpowerDao superpowerDao;
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+
+    @Before
+    public void setUp() {
+
+        List<Location> locations = locationDao.getAllLocations();
+        for(Location loc: locations){
+            locationDao.deleteLocationById(loc.getId());
+        }
+
+        List<Organization> organizations = organizationDao.getAllOrganizations();
+        for(Organization org: organizations){
+            superhumanDao.deleteSuperhumanById(org.getId());
+        }
+
+        List<Superhuman> superhumans = superhumanDao.getAllSuperhumans();
+        for(Superhuman sh: superhumans) {
+            superhumanDao.deleteSuperhumanById(sh.getId());
+        }
+
+        List<Superpower> superpowers = superpowerDao.getAllSuperpowers();
+        for(Superpower sp: superpowers){
+            superpowerDao.deleteSuperpowerById(sp.getId());
+        }
+    }
+
+
+}
---
+++ src/test/java/com/sg/superhumanSightings/data/SuperhumanDaoDBTest.java
@@ -0,0 +1,72 @@
+package com.sg.superhumanSightings.data;
+
+import com.sg.superhumanSightings.dao.*;
+import com.sg.superhumanSightings.entity.Location;
+import com.sg.superhumanSightings.entity.Organization;
+import com.sg.superhumanSightings.entity.Superhuman;
+import com.sg.superhumanSightings.entity.Superpower;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SuperhumanDaoDBTest {
+
+
+    @Autowired
+    LocationDao locationDao;
+
+    @Autowired
+    OrganizationDao organizationDao;
+
+    @Autowired
+    SightingDao sightingDao;
+
+    @Autowired
+    SuperhumanDao superhumanDao;
+
+    @Autowired
+    SuperpowerDao superpowerDao;
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+
+    @Before
+    public void setUp() {
+
+        List<Location> locations = locationDao.getAllLocations();
+        for(Location loc: locations){
+            locationDao.deleteLocationById(loc.getId());
+        }
+
+        List<Organization> organizations = organizationDao.getAllOrganizations();
+        for(Organization org: organizations){
+            superhumanDao.deleteSuperhumanById(org.getId());
+        }
+
+        List<Superhuman> superhumans = superhumanDao.getAllSuperhumans();
+        for(Superhuman sh: superhumans) {
+            superhumanDao.deleteSuperhumanById(sh.getId());
+        }
+
+        List<Superpower> superpowers = superpowerDao.getAllSuperpowers();
+        for(Superpower sp: superpowers){
+            superpowerDao.deleteSuperpowerById(sp.getId());
+        }
+    }
+
+
+}
