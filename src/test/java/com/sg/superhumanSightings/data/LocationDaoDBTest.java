cd21c6d91fbfaffc2f4131035a14707063f4f435
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-10 16:49:12 -0500

    Rename Location test class

--- src/test/java/com/sg/superhumanSightings/data/LocationDaoDBTest.java
+++ src/test/java/com/sg/superhumanSightings/data/LocationDaoDBTest.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LocationDaoDBTEST {
+public class LocationDaoDBTest {
 
     @Autowired
     LocationDao locationDao;
