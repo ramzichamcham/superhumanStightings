2613af047eb71c3a1188f18bb69b8f68e0b7be95
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-11 11:15:21 -0500

    Add ServiceLayer and add localDateTimeToSQLDate method

---
+++ src/main/java/com/sg/superhumanSightings/ServiceLayer/SuperhumanSightingsServiceImplementation.java
@@ -0,0 +1,16 @@
+package com.sg.superhumanSightings.ServiceLayer;
+
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+@Service
+public class SuperhumanSightingsServiceImplementation implements SuperhumanSightingsServiceLayer{
+
+    @Override
+    public Date localDateTimetoSQLDate(LocalDateTime dateTime) {
+        java.sql.Date sqlDate = java.sql.Date.valueOf(dateTime.toLocalDate());
+        return sqlDate;
+    }
+}
--- src/main/java/com/sg/superhumanSightings/ServiceLayer/SuperhumanSightingsServiceLayer.java
+++ src/main/java/com/sg/superhumanSightings/ServiceLayer/SuperhumanSightingsServiceLayer.java
@@ -1,4 +1,8 @@
 package com.sg.superhumanSightings.ServiceLayer;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+
 public interface SuperhumanSightingsServiceLayer {
+    public Date localDateTimetoSQLDate(LocalDateTime dateTime);
 }
