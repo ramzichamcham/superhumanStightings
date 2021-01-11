6378bef48ae687fc7b7223ac7fd78c13c994adcf
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-11 15:22:14 -0500

    testGetSuperhumansForLocation

--- src/test/java/com/sg/superhumanSightings/data/SightingDaoDBTest.java
+++ src/test/java/com/sg/superhumanSightings/data/SightingDaoDBTest.java
@@ -75,7 +75,7 @@ public class SightingDaoDBTest {
 
     @Test
     public void testAddSightingAndGetForDate(){
-        //create new sh
+
         //create new superhuman
         Superhuman sh = new Superhuman();
         sh.setName("Superman");
@@ -134,8 +134,109 @@ public class SightingDaoDBTest {
         assertEquals(1, fromDaoForDate.size());
         assertEquals(sighting, fromDaoForDate.get(0));
         assertTrue(fromDaoForDate.contains(sighting));
+    }
 
+    @Test
+    public void testGetSuperhumansForLocation(){
 
+        //create new superhuman
+        Superhuman sh = new Superhuman();
+        sh.setName("Superman");
+        sh.setDescription("A man that is super");
+
+        //add superpowers to sh
+        List<Superpower> superpowers = new ArrayList<>();
+        Superpower power = new Superpower();
+        power.setName("fly");
+        power.setDescription("Ability to fly");
+        superpowerDao.addSuperpower(power);
+        superpowers.add(power);
+        sh.setSuperpowers(superpowers);
+
+        //add organizations to sh
+        List<Organization> organizations = new ArrayList<>();
+        Organization org = new Organization();
+        org.setName("Super people institute");
+        org.setAddress("Cambridge, MA, United States");
+        org.setDescription("Learn to fly");
+        org.setEmail("superpeople@gmail.com");
+        org.setPhoneNumber("415-290-7907");
+        organizationDao.addOrganization(org);
+        organizations.add(org);
+        sh.setOrganizations(organizations);
+
+        superhumanDao.addSuperhuman(sh);
+
+        //create another superhuman
+        Superhuman sh2 = new Superhuman();
+        sh2.setName("spiderman");
+        sh2.setDescription("A man with spider abilities");
+
+        //add superpowers to sh
+        List<Superpower> superpowers2 = new ArrayList<>();
+        Superpower power2 = new Superpower();
+        power2.setName("swing");
+        power2.setDescription("Ability to swing");
+        superpowerDao.addSuperpower(power2);
+        superpowers2.add(power2);
+        sh2.setSuperpowers(superpowers2);
+
+        //add organizations to sh
+        List<Organization> organizations2 = new ArrayList<>();
+        Organization org2 = new Organization();
+        org2.setName("Spider people institute");
+        org2.setAddress("Cambridge, MA, United States");
+        org2.setDescription("Learn to swing");
+        org2.setEmail("spiderpeople@gmail.com");
+        org2.setPhoneNumber("415-290-7908");
+        organizationDao.addOrganization(org2);
+        organizations2.add(org2);
+        sh2.setOrganizations(organizations2);
+
+        superhumanDao.addSuperhuman(sh2);
+
+        //create 2 locations
+        Location loc = new Location();
+        loc.setName("Times Square");
+        loc.setAddress("Manhattan, NY 10036, United States");
+        loc.setLongitude(-73.985130);
+        loc.setLatitude(40.758896);
+        loc.setDescription("Famous central square in the Theater District of Manhattan");
+
+        locationDao.addLocation(loc);
+
+//        Location loc2 = new Location();
+//        loc2.setName("The Bridge");
+//        loc2.setAddress("Manhattan, NY 00451, United States");
+//        loc2.setLongitude(-73.985130);
+//        loc2.setLatitude(40.758896);
+//        loc2.setDescription("A really big bridge");
+//
+//        locationDao.addLocation(loc2);
+
+        //add 2 superhuman sightings for same location
+        LocalDateTime dateTime = service.localDateTimeNow();
+        Sighting sighting = new Sighting();
+        sighting.setLocation(loc);
+        sighting.setSuperhuman(sh);
+        sighting.setDateTime(dateTime);
+
+        sightingDao.addSighting(sighting);
+
+        LocalDateTime dateTime2 = service.localDateTimeNow();
+        Sighting sighting2 = new Sighting();
+        sighting2.setLocation(loc);
+        sighting2.setSuperhuman(sh2);
+        sighting2.setDateTime(dateTime2);
+
+        sightingDao.addSighting(sighting2);
+        //getSuperhumansForLocation
+        List<Superhuman> shForLocationFromDao = sightingDao.getSuperhumansForLocation(loc);
+
+        //assert
+        assertEquals(2, shForLocationFromDao.size());
+        assertTrue(shForLocationFromDao.contains(sh));
+        assertTrue(shForLocationFromDao.contains(sh2));
     }
 
 
