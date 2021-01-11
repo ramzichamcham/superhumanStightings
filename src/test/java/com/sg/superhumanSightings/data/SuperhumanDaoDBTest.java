6679d0a41480272b310eadfc370fb7a08fe82a26
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-11 12:51:22 -0500

    Update SuperhumanDaoDBTest.java

--- src/test/java/com/sg/superhumanSightings/data/SuperhumanDaoDBTest.java
+++ src/test/java/com/sg/superhumanSightings/data/SuperhumanDaoDBTest.java
@@ -8,11 +8,14 @@ import com.sg.superhumanSightings.entity.Superpower;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -66,6 +69,42 @@ public class SuperhumanDaoDBTest {
         for(Superpower sp: superpowers){
             superpowerDao.deleteSuperpowerById(sp.getId());
         }
+    }
+
+    @Test
+    public void testAddAndGetSuperhuman(){
+        //create new superhuman
+        Superhuman sh = new Superhuman();
+        sh.setName("Superman");
+        sh.setDescription("A man that is super");
+
+        //add superpowers to sh
+        List<Superpower> superpowers = new ArrayList<>();
+        Superpower power = new Superpower();
+        power.setName("fly");
+        power.setDescription("Ability to fly");
+        superpowerDao.addSuperpower(power);
+        superpowers.add(power);
+        sh.setSuperpowers(superpowers);
+
+        //add organizations to sh
+        List<Organization> organizations = new ArrayList<>();
+        Organization org = new Organization();
+        org.setName("Super people institute");
+        org.setAddress("Cambridge, MA, United States");
+        org.setDescription("Learn to fly");
+        org.setEmail("superpeople@gmail.com");
+        org.setPhoneNumber("415-290-7907");
+        organizationDao.addOrganization(org);
+        organizations.add(org);
+        sh.setOrganizations(organizations);
+
+        superhumanDao.addSuperhuman(sh);
+
+        Superhuman shFromDao = superhumanDao.getSuperhumanById(sh.getId());
+
+        assertEquals(sh, shFromDao);
+
     }
 
 
