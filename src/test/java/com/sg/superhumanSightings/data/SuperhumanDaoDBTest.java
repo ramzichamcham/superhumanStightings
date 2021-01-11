39be86b21202c9d9d6862895fdc6334183de3ec6
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-11 12:57:45 -0500

    Update SuperhumanDaoDBTest.java

--- src/test/java/com/sg/superhumanSightings/data/SuperhumanDaoDBTest.java
+++ src/test/java/com/sg/superhumanSightings/data/SuperhumanDaoDBTest.java
@@ -107,5 +107,71 @@ public class SuperhumanDaoDBTest {
 
     }
 
+    @Test
+    public void testGetAllSuperhumans() {
+
+//create new superhuman
+        Superhuman sh = new Superhuman();
+        sh.setName("Superman");
+        sh.setDescription("A man that is super");
+
+        //add superpowers to sh
+        List<Superpower> superpowers = new ArrayList<>();
+        Superpower power = new Superpower();
+        power.setName("fly");
+        power.setDescription("Ability to fly");
+        superpowerDao.addSuperpower(power);
+        superpowers.add(power);
+        sh.setSuperpowers(superpowers);
+
+        //add organizations to sh
+        List<Organization> organizations = new ArrayList<>();
+        Organization org = new Organization();
+        org.setName("Super people institute");
+        org.setAddress("Cambridge, MA, United States");
+        org.setDescription("Learn to fly");
+        org.setEmail("superpeople@gmail.com");
+        org.setPhoneNumber("415-290-7907");
+        organizationDao.addOrganization(org);
+        organizations.add(org);
+        sh.setOrganizations(organizations);
+
+        superhumanDao.addSuperhuman(sh);
+
+        //create another superhuman
+        Superhuman sh2 = new Superhuman();
+        sh2.setName("spiderman");
+        sh2.setDescription("A man with spider abilities");
+
+        //add superpowers to sh
+        List<Superpower> superpowers2 = new ArrayList<>();
+        Superpower power2 = new Superpower();
+        power2.setName("swing");
+        power2.setDescription("Ability to swing");
+        superpowerDao.addSuperpower(power2);
+        superpowers2.add(power2);
+        sh2.setSuperpowers(superpowers2);
+
+        //add organizations to sh
+        List<Organization> organizations2 = new ArrayList<>();
+        Organization org2 = new Organization();
+        org2.setName("Spider people institute");
+        org2.setAddress("Cambridge, MA, United States");
+        org2.setDescription("Learn to swing");
+        org2.setEmail("spiderpeople@gmail.com");
+        org2.setPhoneNumber("415-290-7908");
+        organizationDao.addOrganization(org2);
+        organizations2.add(org2);
+        sh2.setOrganizations(organizations2);
+
+        superhumanDao.addSuperhuman(sh2);
+
+        List<Superhuman> superhumans = superhumanDao.getAllSuperhumans();
+
+        assertEquals(2, superhumans.size());
+        assertTrue(superhumans.contains(sh));
+        assertTrue(superhumans.contains(sh2));
+    }
+
 
 }
