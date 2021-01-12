42ced1325adc357312923e2037ac308d4ae34f07
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-12 16:20:22 -0500

    Add add organization feature

--- src/main/java/com/sg/superhumanSightings/controller/OrganizationController.java
+++ src/main/java/com/sg/superhumanSightings/controller/OrganizationController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -22,5 +24,26 @@ public class OrganizationController {
         return "organizations";
 
     }
+
+    @PostMapping("addOrganization")
+    public String addOrganization(HttpServletRequest request){
+        String name = request.getParameter("organizationName");
+        String description = request.getParameter("organizationDescription");
+        String address = request.getParameter("organizationAddress");
+        String phoneNumber = request.getParameter("organizationPhoneNumber");
+        String email = request.getParameter("organizationEmail");
+
+        Organization organization = new Organization();
+        organization.setName(name);
+        organization.setDescription(description);
+        organization.setAddress(address);
+        organization.setPhoneNumber(phoneNumber);
+        organization.setEmail(email);
+
+        organizationDao.addOrganization(organization);
+
+        return "redirect:/organizations";
+    }
+
 
 }
