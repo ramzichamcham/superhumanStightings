7344a9e2e862c0d1f35f871a0a0f84dec1f8db1c
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-12 19:15:12 -0500

    Add add feature to superhumans

--- src/main/java/com/sg/superhumanSightings/controller/SuperhumanController.java
+++ src/main/java/com/sg/superhumanSightings/controller/SuperhumanController.java
@@ -1,12 +1,19 @@
 package com.sg.superhumanSightings.controller;
 
+import com.sg.superhumanSightings.dao.OrganizationDao;
 import com.sg.superhumanSightings.dao.SuperhumanDao;
+import com.sg.superhumanSightings.dao.SuperpowerDao;
+import com.sg.superhumanSightings.entity.Organization;
 import com.sg.superhumanSightings.entity.Superhuman;
+import com.sg.superhumanSightings.entity.Superpower;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -15,10 +22,53 @@ public class SuperhumanController {
     @Autowired
     SuperhumanDao superhumanDao;
 
+    @Autowired
+    SuperpowerDao superpowerDao;
+
+    @Autowired
+    OrganizationDao organizationDao;
+
     @GetMapping("superhumans")
     public String displaySuperhumans(Model model){
         List<Superhuman> superhumans = superhumanDao.getAllSuperhumans();
+        List<Superpower> superpowers = superpowerDao.getAllSuperpowers();
+        List<Organization> organizations = organizationDao.getAllOrganizations();
+
         model.addAttribute("superhumans", superhumans);
+        model.addAttribute("superpowers", superpowers);
+        model.addAttribute("organizations", organizations);
         return "superhumans";
+    }
+
+
+    @PostMapping("addSuperhuman")
+    public String addSuperhuman(HttpServletRequest request){
+        Superhuman superhuman = new Superhuman();
+
+        String name = request.getParameter("superhumanName");
+        String description = request.getParameter("superhumanDescription");
+
+        String[] superpowerIds = request.getParameterValues("superhumanSuperpowers");
+        String[] organizationIds = request.getParameterValues("superhumanOrganizations");
+
+        List<Superpower> superpowers = new ArrayList<>();
+        for(String superpowerId: superpowerIds){
+            superpowers.add(superpowerDao.getSuperpowerById(Integer.parseInt(superpowerId)));
+        }
+
+        List<Organization> organizations = new ArrayList<>();
+        for(String organizationId: organizationIds){
+            organizations.add(organizationDao.getOrganizationById(Integer.parseInt(organizationId)));
+        }
+
+        superhuman.setName(name);
+        superhuman.setDescription(description);
+        superhuman.setSuperpowers(superpowers);
+        superhuman.setOrganizations(organizations);
+
+        superhumanDao.addSuperhuman(superhuman);
+
+        return "redirect:/superhumans";
+
     }
 }
