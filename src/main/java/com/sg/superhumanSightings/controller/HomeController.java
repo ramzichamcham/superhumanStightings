56b4143549799b20a44a6ad2e410bba82da294fc
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-13 16:14:52 -0500

    add most recent sightings and redirect to index page

--- src/main/java/com/sg/superhumanSightings/controller/HomeController.java
+++ src/main/java/com/sg/superhumanSightings/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.sg.superhumanSightings.controller;
 
 import com.sg.superhumanSightings.dao.SightingDao;
+import com.sg.superhumanSightings.entity.Sighting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -12,11 +15,12 @@ public class HomeController {
     @Autowired
     SightingDao sightingDao;
 
-    @GetMapping("index")
+    @GetMapping("/")
     public String displayHomePage(Model model){
-        //get 10 most recent sightings to display in table
 
-        //
+        List<Sighting> sightings = sightingDao.getRecentSightings(10);
+
+        model.addAttribute("sightings", sightings);
         return "index";
     }
 }
