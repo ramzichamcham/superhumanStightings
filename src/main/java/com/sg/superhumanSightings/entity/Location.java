3b1df3caec6d7d1c5731a4b751e7266659fa3284
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-14 11:25:38 -0500

    modify Location for validation

--- src/main/java/com/sg/superhumanSightings/entity/Location.java
+++ src/main/java/com/sg/superhumanSightings/entity/Location.java
@@ -1,14 +1,27 @@
 package com.sg.superhumanSightings.entity;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Location {
     private int id;
     private String name;
+    @NotBlank(message="Address must not be empty.")
+    @Size(max = 100, message = "Address must be less than 100 characters.")
     private String address;
+    @Size(max = 200, message = "Description must be less than 200 characters.")
     private String description;
-    private double latitude;
-    private double longitude;
+
+    @NotNull(message = "Latitude must not be empty")
+    @Digits(integer = 2, fraction = 6, message = "Latitude must have 2 integral and 6 fractional digits at most. ")
+    private Double latitude;
+
+    @NotNull(message="Longitude must not be empty")
+    @Digits(integer = 3, fraction = 6, message = "Longitude must have 3 integral and 6 fractional digits at most. ")
+    private Double longitude;
 
     public int getId() {
         return id;
@@ -42,34 +55,35 @@ public class Location {
         this.description = description;
     }
 
-    public double getLatitude() {
+    public Double getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(double latitude) {
+    public void setLatitude(Double latitude) {
         this.latitude = latitude;
     }
 
-    public double getLongitude() {
+    public Double getLongitude() {
         return longitude;
     }
 
-    public void setLongitude(double longitude) {
+    public void setLongitude(Double longitude) {
         this.longitude = longitude;
     }
 
     //name and description can be null
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Location location = (Location) o;
         return id == location.id &&
-                Double.compare(location.latitude, latitude) == 0 &&
-                Double.compare(location.longitude, longitude) == 0 &&
                 Objects.equals(name, location.name) &&
                 address.equals(location.address) &&
-                Objects.equals(description, location.description);
+                Objects.equals(description, location.description) &&
+                latitude.equals(location.latitude) &&
+                longitude.equals(location.longitude);
     }
 
     @Override
